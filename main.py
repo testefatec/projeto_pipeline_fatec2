a3f821e4a9909319554bc1c3f55af0d106c09374
Blackout <sergiogabriel2511g@gmail.com>
2026-05-23 22:22:30 -0300

    test: código vulnerável - SQL Injection

--- main.py
+++ main.py
@@ -23,14 +23,9 @@ if __name__ == "__main__":
 import sqlite3
 
 def buscar_usuario_vulneravel(user_id):
-
     conn = sqlite3.connect('banco.db')
-
     cursor = conn.cursor()
-
     # ⚠️ SQL INJECTION: nunca faça isso em produção!
-
     cursor.execute(f"SELECT * FROM users WHERE id={user_id}")
-
     return cursor.fetchone()
 
